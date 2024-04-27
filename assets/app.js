99a0e37a664b75b493f82ca41b97885587546abb
mmarchois <mathieu.marchois@gmail.com>
2024-04-27 15:05:48 +0200

    Add and configure flowbite

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import "./styles/app.scss";
+import "./styles/app.css";
 
 // start the Stimulus application
 import "./bootstrap";
+
+// enable the interactive UI components from Flowbite with Turbo
+import 'flowbite/dist/flowbite.turbo.js';
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     .setPublicPath('/build')
+    .enablePostCssLoader()
 
     /*
      * ENTRY CONFIG
